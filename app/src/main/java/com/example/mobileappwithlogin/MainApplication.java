0cc0f734ce4d24cefbeb2313b547cbb9fba8d0e2
amarose <adammaroszek1@gmail.com>
2023-03-28 15:17:19 +0200

    cleanup and update deps

--- app/src/main/java/com/example/mobileappwithlogin/MainApplication.java
+++ app/src/main/java/com/example/mobileappwithlogin/MainApplication.java
@@ -2,6 +2,7 @@ package com.example.mobileappwithlogin;
 
 
 import android.app.Application;
+import android.util.Log;
 
 import com.pushpushgo.sdk.PushPushGo;
 
@@ -9,6 +10,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("TEST", "initialize");
         // Instantiate PPG sdk in main app
         PushPushGo.getInstance(this);
     }
--- app/src/main/java/com/example/mobileappwithlogin/activities/MainActivity.java
+++ app/src/main/java/com/example/mobileappwithlogin/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -46,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Register user for subscription to notifications
         ppg.registerSubscriber();
+        Log.d("PushPushGoUserToken", ppg.getSubscriberId());
 
         // Selectors and on click listeners
         TextView register = findViewById(R.id.registerUserButton);
